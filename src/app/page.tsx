f08aa31f375872c9d4bf3a448bb916afbe9f71fb
aureliome <6731624+aureliome@users.noreply.github.com>
2024-05-02 18:10:55 +0200

    feat: get description from an image using OpenAI APIs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,38 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import paintings from "@/data/paintings/paintings";
+import {
+  getDescriptionFromAnImage,
+  generateImageFromPrompt,
+} from "@/data/openai";
 import PaintingItem from "./components/PaintingItem";
 
 export default function Home() {
   const [selectedPainting, setSelectedPaiting] = useState<null | Painting>(
     null
   );
+  const [imageDescription, setImageDescription] = useState<null | any>(null);
 
   function selectPainting(painting: Painting) {
     setSelectedPaiting(painting);
   }
+
+  useEffect(() => {
+    const fetchImageDescription = async (imageUrl: string) => {
+      const data = await getDescriptionFromAnImage({
+        imageUrl,
+      });
+      const json = await data.json();
+      console.log(json);
+      setImageDescription(json);
+    };
+
+    console.log("useEffect - selectedPaiting", selectedPainting);
+    if (selectedPainting) {
+      fetchImageDescription(selectedPainting.image);
+    }
+  }, [selectedPainting]);
 
   return (
     <main>
@@ -33,6 +54,8 @@ export default function Home() {
           />
         ))
       )}
+
+      {imageDescription && <p>{JSON.stringify(imageDescription)}</p>}
     </main>
   );
 }
