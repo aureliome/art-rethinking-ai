9ae011f5c11809a1e70585e7c380823b89e4e67c
aureliome <6731624+aureliome@users.noreply.github.com>
2024-05-02 16:01:18 +0200

    feat: added list of paintings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
+"use client";
+
+import paintings from "@/data/paintings/paintings";
+import style from "./page.module.css";
+
 export default function Home() {
+  function printPaintingData(index: number) {
+    console.log(paintings[index]);
+  }
+
   return (
     <main>
       <h1>Art Rethinking AI</h1>
@@ -7,6 +16,24 @@ export default function Home() {
       <p>
         Click on one of the following artwork you want to <i>rethink</i>
       </p>
+
+      {paintings.map((painting, index) => (
+        <div
+          className={style.painting}
+          onClick={() => printPaintingData(index)}
+          key={painting.id}
+        >
+          <img src={painting.image} alt={painting.title} height="100" />
+          <div>
+            <p>
+              <strong>{painting.title}</strong>
+            </p>
+            <p>
+              <i>{painting.artistName}</i>
+            </p>
+          </div>
+        </div>
+      ))}
     </main>
   );
 }
