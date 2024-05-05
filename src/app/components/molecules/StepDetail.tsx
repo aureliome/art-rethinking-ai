9178ba140608aeb93e6285d515c1c2560b34ee70
aureliome <6731624+aureliome@users.noreply.github.com>
2024-05-05 23:38:05 +0200

    chore: added "show detail" button in StepDetail

--- src/app/components/molecules/StepDetail.tsx
+++ src/app/components/molecules/StepDetail.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { useState } from "react";
 
 export default function StepDetail({
   request,
@@ -7,16 +7,33 @@ export default function StepDetail({
   request: React.ReactNode;
   response: React.ReactNode;
 }) {
+  const [showDetail, setShowDetail] = useState<boolean>(false);
+
   return (
-    <div className="row">
-      <div className="col s12 m6">
-        <h6>REQUEST</h6>
-        {request}
-      </div>
-      <div className="col s12 m6">
-        <h6>RESPONSE</h6>
-        {response}
-      </div>
+    <div>
+      {showDetail ? (
+        <div className="row">
+          <div className="col s12 m6">
+            <h6>REQUEST</h6>
+            {request}
+          </div>
+          <div className="col s12 m6">
+            <h6>RESPONSE</h6>
+            {response}
+          </div>
+        </div>
+      ) : (
+        <p>
+          <a
+            className="btn-flat teal-text"
+            onClick={() => {
+              setShowDetail(true);
+            }}
+          >
+            Show detail
+          </a>
+        </p>
+      )}
     </div>
   );
 }
