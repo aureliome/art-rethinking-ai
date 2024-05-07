f5f9a3a717333e759a1688f6ebd903a5e7e11c40
aureliome <6731624+aureliome@users.noreply.github.com>
2024-05-07 10:46:21 +0200

    chore: created StepAsyncContent component

---
+++ src/app/components/molecules/StepAsyncContent.tsx
@@ -0,0 +1,44 @@
+import { KeyedMutator } from "swr";
+
+export default function StepAsyncContent({
+  children,
+  data,
+  error,
+  isLoading,
+  onRetry,
+}: {
+  children: React.ReactNode;
+  data: any;
+  error: any;
+  isLoading: boolean;
+  onRetry: KeyedMutator<any>;
+}) {
+  return (
+    <div>
+      {isLoading && (
+        <p className="center-align">
+          <i>
+            LOADING...
+            <br />
+            it could take some seconds
+          </i>
+        </p>
+      )}
+
+      {error && (
+        <>
+          <div className="center-align">
+            <i className="red-text">An error occured</i>
+          </div>
+          <div className="center-align">
+            <button className="btn-small" onClick={onRetry}>
+              RETRY
+            </button>
+          </div>
+        </>
+      )}
+
+      {data && children}
+    </div>
+  );
+}
