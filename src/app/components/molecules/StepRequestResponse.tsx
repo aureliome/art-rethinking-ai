33f9edea89711752fbfd0167a94564a9462d861c
aureliome <6731624+aureliome@users.noreply.github.com>
2024-05-07 10:46:33 +0200

    chore: created StepRequestResponse component

---
+++ src/app/components/molecules/StepRequestResponse.tsx
@@ -0,0 +1,20 @@
+export default function StepRequestResponse({
+  request,
+  response,
+}: {
+  request: React.ReactNode;
+  response: React.ReactNode;
+}) {
+  return (
+    <div className="row">
+      <div className="col s12 m6">
+        <h6>REQUEST</h6>
+        {request}
+      </div>
+      <div className="col s12 m6">
+        <h6>RESPONSE</h6>
+        {response}
+      </div>
+    </div>
+  );
+}
